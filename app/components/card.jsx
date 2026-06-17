0016e3649c7219256f8e2a8118eb86a66ec32f55
Manish Raje <manishbraje@gmail.com>
2026-06-18 02:23:46 +0530

    added cards and did them using component like simple charts

---
+++ app/components/card.jsx
@@ -0,0 +1,59 @@
+// src/components/Card.jsx
+
+import Button from '@mui/joy/Button';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import CardActions from '@mui/joy/CardActions';
+import CircularProgress from '@mui/joy/CircularProgress';
+import Typography from '@mui/joy/Typography';
+import SvgIcon from '@mui/joy/SvgIcon';
+
+export default function DashboardCard({
+     title,
+  value,
+  progress,
+}) {
+  return (
+    <Card variant="solid" color="primary" invertedColors>
+      <CardContent orientation="horizontal">
+        <CircularProgress size="lg" determinate value={progress}>
+          <SvgIcon>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
+              />
+            </svg>
+          </SvgIcon>
+        </CircularProgress>
+
+        <CardContent>
+          <Typography level="body-md">
+            {title}
+          </Typography>
+
+          <Typography level="h2">
+            {value}
+          </Typography>
+        </CardContent>
+      </CardContent>
+
+      <CardActions>
+        <Button variant="soft" size="sm">
+          View Details
+        </Button>
+
+        <Button variant="solid" size="sm">
+          Analytics
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
